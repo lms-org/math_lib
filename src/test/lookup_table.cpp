9d825477d2dfe9523bf69d3fb782471cf4a364c9
Hans Kirchner <hans-kirchner@gmx.de>
2015-05-13 12:46:44 +0200

    Add more test cases

--- src/test/lookup_table.cpp
+++ src/test/lookup_table.cpp
@@ -23,11 +23,13 @@ TEST(LookupTable, binarySearchFloat) {
     lms::math::linearInterpolation<float>(270, 150, 209, 200, 240, yExpect);
     EXPECT_FLOAT_EQ(yExpect, y);
 
+    // out of range
     result = lookupTableBinarySearch<float, LookupTableOrder::DESC>(vx, vy, 706, y);
     EXPECT_FALSE(result);
 
     result = lookupTableBinarySearch<float, LookupTableOrder::DESC>(vx, vy, 65, y);
-    EXPECT_FALSE(result);
+    EXPECT_TRUE(result);
+    EXPECT_FLOAT_EQ(600, y);
 }
 
 TEST(LookupTable, binarySearchInt) {
@@ -38,13 +40,26 @@ TEST(LookupTable, binarySearchInt) {
                                    97,  83,  70,  65,  65,  60,  50,  42};
     const std::vector<int> vy = {0,  50, 100, 150, 200, 250, 300, 350, 400,
                                    450, 500, 550, 600, 650, 700, 750, 800};
-    int y, yExpect;
+    int y;
     bool result;
 
     result = lookupTableBinarySearch<int, LookupTableOrder::DESC>(vx, vy, 270, y);
     EXPECT_TRUE(result);
     EXPECT_FLOAT_EQ(150, y);
 
+    result = lookupTableBinarySearch<int, LookupTableOrder::DESC>(vx, vy, 55, y);
+    EXPECT_TRUE(result);
+    EXPECT_FLOAT_EQ(725, y);
+
+    // NOTE: 65 is duplicated in vx, expect first corresponding vy value
     result = lookupTableBinarySearch<int, LookupTableOrder::DESC>(vx, vy, 65, y);
+    EXPECT_TRUE(result);
+    EXPECT_FLOAT_EQ(600, y);
+
+    // out of range
+    result = lookupTableBinarySearch<int, LookupTableOrder::DESC>(vx, vy, 706, y);
+    EXPECT_FALSE(result);
+
+    result = lookupTableBinarySearch<int, LookupTableOrder::DESC>(vx, vy, 0, y);
     EXPECT_FALSE(result);
 }
