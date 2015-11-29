b87204a92a2b11379423f3f080cf370030189b61
phibedy <philipp.schmette@gmx.de>
2015-11-29 20:46:42 +0100

    fixed rect contains method

--- include/lms/math/math.h
+++ include/lms/math/math.h
@@ -1,21 +1,23 @@
 #ifndef LMS_MATH_MATH_H
 #define LMS_MATH_MATH_H
+#include <iostream>
 
 namespace lms {
 namespace math {
 template <typename T> int sgn(T val) {
     return (T(0) < val) - (val < T(0));
 }
-
+/**
+ * @brief The Rect struct give left, bot corner
+ */
 struct Rect{
     float x;
     float y;
     float width;
     float height;
     bool contains(float _x, float _y){
-        return x >= _x && _y >= y && _x <= x+width && _y <= y+height;
+        return _x >= x && _y >= y && _x <= x+width && _y <= y+height;
     }
-
 };
 
 /**
