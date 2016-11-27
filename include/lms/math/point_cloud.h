232b54882bd125f46f22bf6a68c413a649f6f463
Arthur Mathies <arthur.mathies@googlemail.com>
2016-11-27 18:42:10 +0100

    Add setter for point vector in point cloud

--- include/lms/math/point_cloud.h
+++ include/lms/math/point_cloud.h
@@ -1,6 +1,7 @@
 #ifndef LMS_MATH_POINT_CLOUD_H
 #define LMS_MATH_POINT_CLOUD_H
 
+#include <utility>
 #include <vector>
 
 #include "lms/math/vertex.h"
@@ -13,6 +14,7 @@ class PointCloud {
    public:
     const std::vector<V>& points() const { return m_points; }
     std::vector<V>& points() { return m_points; }
+    void points(std::vector<V> newPoints) { m_points = std::move(newPoints); }
 
     void clear() { m_points.clear(); }
     typename std::vector<V>::size_type size() const { return m_points.size(); }
